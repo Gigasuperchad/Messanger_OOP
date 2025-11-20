31d782d4c132646db5614b874836b94c7bf5b613
stepan <p.stepashka2006@gmail.com>
2025-11-20 11:47:07 +1000

    Create Project

---
+++ src/main/java/com/example/messanger_oop/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.messanger_oop;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.messanger_oop {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.messanger_oop to javafx.fxml;
+    exports com.example.messanger_oop;
+}
